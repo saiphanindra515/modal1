eb8e006f261d669e26033271d45355546e36823a
saiphanindra515 <30469122+saiphanindra515@users.noreply.github.com>
2018-04-16 14:48:01 +0530

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+$(".overlay").hide();
+$(".modalDesign").hide();
+
+$(".modaltrigger").click(function(){
+
+$(".overlay").show();
+$(".modalDesign").show();
+
+});
+
+$("#close").click(function(){
+
+$(".overlay").hide();
+$(".modalDesign").hide();
+
+});
+$(".overlay").click(function(){
+
+	$(".overlay").hide();
+	$(".modalDesign").hide();
+})
+
+
+
+	
+
+
+
+
+});
